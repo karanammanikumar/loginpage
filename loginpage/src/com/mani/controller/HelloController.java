41bb9854273de03cceb2e4daaa90c0940e9e454e
karanammanikumar <karanamm3@gmail.com>
2016-08-22 16:34:31 +0530

    new word entered

--- loginpage/src/com/mani/controller/HelloController.java
+++ loginpage/src/com/mani/controller/HelloController.java
@@ -2,7 +2,7 @@ package com.mani.controller;
 
 public class HelloController {
 	public static void main(String[] args) {
-		// comment is added to the file
+		// comment is added to the file entered
 		System.out.println("Hello Git");
 		System.out.println("How are you?");
 		System.out.println("Mani Kumar");
