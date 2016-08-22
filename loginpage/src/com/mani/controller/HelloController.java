edee3b05129a9c286954a0afddabc38427ba68ec
Karanam <mani@gmail.com>
2016-08-22 14:02:20 +0530

    new line and comment is added

--- loginpage/src/com/mani/controller/HelloController.java
+++ loginpage/src/com/mani/controller/HelloController.java
@@ -2,7 +2,9 @@ package com.mani.controller;
 
 public class HelloController {
 	public static void main(String[] args) {
+		// comment is added to the file
 		System.out.println("Hello Git");
+		System.out.println("How are you?");
 	}
 
 }
